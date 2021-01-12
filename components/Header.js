83ef448ed902eaef516fc67a92df8e44829443f2
vojta <vojta.cina@seznam.cz>
2021-01-12 17:03:30 +0100

    optimalization for mobile phone

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,33 @@
-const Item = ({title, active}) => {
+import React, { useEffect, useState } from "react";
+
+const Item = ({ title, active }) => {
     return (
-        <div className={"m-4 p-4 rounded "+ (active ? "bg-gray-100 text-black font-bold" : "bg-white hover:bg-gray-100 text-gray-800 ")} > 
-                {title}
-            </div>
-    ) }
+        <div className={"m-4 p-4 rounded " + (active ? "bg-gray-100 text-black font-bold" : "bg-white hover:bg-gray-100 text-gray-800 ")} >
+            {title}
+        </div>
+    )
+}
 
 const Header = () => {
+
+    const [opened, setOpened] = useState(false)
+
     return (
-        <div className="justify-start bg-white px-24 py-6 flex flex-center ">
+        <div>
+            <div className="hidden md:flex justify-start bg-white px-6 md:px-24 py-6">
                 <Item title="🔮  Test dle typu osobnosti" active={true} />
-                <Item title="dfgdfg" active={false} />
+                <Item title="💎  Test dle hodnotového žebříčku" active={false} />
             </div>
-    ) }
+            {!opened ? <div className="block md:hidden justify-start bg-white px-6 md:px-24 py-6">
+                <a className="cursor-pointer" onClick={() => setOpened(!opened)}><Item title="⬇ Otevřít menu" active={false} /></a>
+            </div> : <div className="block md:hidden justify-start bg-white px-6 md:px-24 py-6">
+                    <a className="cursor-pointer"  onClick={() => setOpened(!opened)}><Item title="⬆ Zavřít menu" active={false} /></a>
+                    <Item title="🔮  Test dle typu osobnosti" active={true} />
+                    <Item title="💎  Test dle hodnotového žebříčku" active={false} />
+                </div>}
+
+        </div>
+    )
+}
 
 export default Header
--- components/Teaser.js
+++ components/Teaser.js
@@ -4,8 +4,8 @@ import { Link } from 'react-scroll'
 const Teaser = () => {
     return (
         <div className=" bg-white pb-16">
-                <div className="pb-6 pt-12 container mx-auto px-24">
-                    <h2 className="text-6xl font-bold font-serif text-primary bg-clip-text text-transparent bg-gradient-to-r from-black to-green-500 mb-4">Test kompatibility partnerů</h2>
+                <div className="pb-6 pt-12 container mx-auto px-6 md:px-24">
+                    <h2 className=" text-4xl md:text-6xl font-bold font-serif text-primary bg-clip-text text-transparent bg-gradient-to-r from-black to-green-500 mb-4">Test kompatibility partnerů</h2>
                     <p className="mt-6">Vyzkoušejte si tento nástroj. Vyhodnocení ihned. 😊</p>
                     <Link to="main" spy={true} smooth={true}><button className={"my-4 p-4 rounded bg-green-900 text-white"}>Spočítat</button></Link>
 
